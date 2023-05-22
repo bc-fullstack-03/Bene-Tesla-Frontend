aa0298dbc746484064b2ab5db7d373545511df9f
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-05-21 21:12:48 -0300

    :sparkles: add new features

---
+++ frontend/src/components/Dropzone/Dropzone.tsx
@@ -0,0 +1,41 @@
+import { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import Text from "../Text/Text";
+
+interface DropzoneProps {
+  onFileUploaded: (file: File) => void;
+}
+
+function Dropzone({ onFileUploaded }: DropzoneProps) {
+  const [selectedFileUrl, setSelectedFileUrl] = useState("");
+
+  const onDrop = useCallback(
+    (acceptedFiles: any[]) => {
+      const file = acceptedFiles[0];
+
+      const fileURL = URL.createObjectURL(file);
+
+      setSelectedFileUrl(fileURL);
+      onFileUploaded(file);
+    },
+    [onFileUploaded]
+  );
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div className="flex mt-4" {...getRootProps()}>
+      <input {...getInputProps()} />
+
+      {selectedFileUrl ? (
+        <img src={selectedFileUrl} className="max-h-96 rounded-lg" />
+      ) : (
+        <Text size="sm">
+            Drag your image here
+            ..</Text>
+      )}
+    </div>
+  );
+}
+
+export default Dropzone;
