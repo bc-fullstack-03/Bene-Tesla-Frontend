836a9b06009d4d0a4a22b2c930feeb6a4a7b48cc
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-05-24 13:51:15 -0300

    :art: creating system button.

--- frontend/src/components/Btn/Button.tsx
+++ frontend/src/components/Btn/Button.tsx
@@ -14,7 +14,7 @@ function Button({asChild, children, className, ...props}: ButtonProps) {
     return (
         <Comp 
         className={clsx(
-            'inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-900 hover:bg-gray-700 cursor-pointer',
+            'inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-900 hover:bg-blue-700 cursor-pointer',
             )} 
             {...props}>
             {children}
