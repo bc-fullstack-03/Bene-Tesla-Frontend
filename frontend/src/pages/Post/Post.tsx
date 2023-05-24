a2d0ffdeb1c25046d399515d0cec29de66e617e4
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-05-24 13:45:03 -0300

    :art: this is the page of our post, it contains data that are native to our post

---
+++ frontend/src/pages/Post/Post.tsx
@@ -0,0 +1,12 @@
+import CreatePostButton from "../../components/CreatePostBtn/CreatePostBtn";
+import NavBar from "../../components/Navbar/Navbar";
+
+function Post (){
+    return <>
+    <NavBar />  
+    <  CreatePostButton />
+    </>
+
+}
+
+export default Post;
