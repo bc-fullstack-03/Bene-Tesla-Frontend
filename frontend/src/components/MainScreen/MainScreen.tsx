85bb263cabda5f427a1ec81d5f455875b9175943
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-05-24 13:42:19 -0300

    :sparkles: think of it like a 49 inch tv full of video games, this is our main screen.

--- frontend/src/components/MainScreen/MainScreen.tsx
+++ frontend/src/components/MainScreen/MainScreen.tsx
@@ -1,16 +1,18 @@
 import { ReactNode } from 'react';
 import NavBar from '../../components/Navbar/Navbar';
+import { Post } from '../../model/Post';
 interface MainScreenProps {
     children: ReactNode
+    postCreated?: (post: Post) => void;
 }
 
 function MainScreen(props: MainScreenProps) {
 
-   
+
     return (
         <div className='w-screen h-screen flex '> 
-          <NavBar/>
-          {props.children}
+          <NavBar postCreated={props.postCreated} />
+             {props.children}
         </div>
     )
 
